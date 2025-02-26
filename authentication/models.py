be2ee5752aa3efbc2c2c591d47794c6084ac7c97
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-02-26 21:41:46 +0200

    create models

--- authentication/models.py
+++ authentication/models.py
@@ -1,4 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.email
--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
 
+AUTH_USER_MODEL = 'authentication.User'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
