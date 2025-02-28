e3a176ae040d01a972bd5bb52c39f77011bdc68d
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-02-28 18:28:41 +0200

    Email login  JWT

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,28 +1,47 @@
 from rest_framework import serializers
-from .models import User
 from django.contrib.auth import get_user_model
-
 
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-
     password = serializers.CharField(max_length=128, min_length=8, write_only=True)
 
     class Meta:
         model = User
         fields = ['email', 'username', 'first_name', 'last_name', 'password']
 
-        def create(self , validated_data):
-            user = User(
-                email = validated_data['email'],
-                username = validated_data['username'],
-                first_name = validated_data['first_name'],
-                last_name = validated_data['last_name']
+    def create(self, validated_data):
+        """إنشاء مستخدم جديد مع تشفير كلمة المرور"""
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])  # تشفير كلمة المرور
+        user.save()
+        return user
 
-            )
 
-            user.set_password(validated_data['password'])
-            user.save()
-            return user
-    
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        username_or_email = attrs.get("username")  # هذا الحقل يستقبل البريد أو اسم المستخدم
+        password = attrs.get("password")
+
+        # التحقق مما إذا كان الإدخال بريدًا إلكترونيًا أو اسم مستخدم
+        try:
+            user = User.objects.get(email=username_or_email)
+        except User.DoesNotExist:
+            try:
+                user = User.objects.get(username=username_or_email)
+            except User.DoesNotExist:
+                raise serializers.ValidationError({"error": "No active account found with the given credentials"})
+
+        # التحقق من صحة كلمة المرور
+        if not user.check_password(password):
+            raise serializers.ValidationError({"error": "Incorrect credentials"})
+
+        # تحديث `username` ليتم تمريره للنظام الأساسي لـ JWT
+        attrs["username"] = user.username
+        return super().validate(attrs)
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from .models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer , CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,3 +19,7 @@ class RegisterView(APIView):
             serializer.save()
             return Response({"message": "تم التسجيل بنجاح"} , status = status.HTTP_201_CREATED)
         return Response(serializer.errors , status = status.HTTP_400_BAD_REQUEST)
+    
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
